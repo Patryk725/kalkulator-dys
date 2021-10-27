198073a378088199afb3fc6da9a11a30bd4fde23
Patryk725 <68645604+Patryk725@users.noreply.github.com>
2021-10-27 17:01:28 +0200

    Added some comments to make code more readable

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const toTime=()=>
 {
+    // Tworzę zmienne na podstawie wartości z divów
     let time = document.getElementById("field1").value;
     let velocity = document.getElementById("field2").value;
     let goal = document.getElementById("field3").value;
@@ -8,9 +9,10 @@ const toTime=()=>
     // Jeśli wartość ma być pusta 👇
     // alert(velocity==null||velocity=="")
 
-    // Jeśli wartość ma coś mieć wpisane
+    // Jeśli wartość ma coś mieć wpisane 👇
     // if(time){alert("works")} 
 
+    // Bramki, jeśli dwa inputy są wpisane, a jeden nie
     if(velocity==null||velocity=="" && time && goal)
     {submit.innerHTML="prędkość: "+goal/time+" km/h"}
 
@@ -20,9 +22,11 @@ const toTime=()=>
     else if(goal==null||goal=="" && velocity && time)
     {submit.innerHTML="zostało ci: "+time*velocity+" kilometrów"} 
 
+    //Bramka, jeśli żadne pole nie zostało wypełnione
     else if(velocity==null||velocity=="" && time==null||time=="" && goal==null||goal=="")
     {alert("Nie wypełniłeś żadnego pola")}
     
+    // W przeciwnym razie zaznaczono za dużo pól
     else{alert("Za dużo pól, zostaw jedno puste")}
 
 }
